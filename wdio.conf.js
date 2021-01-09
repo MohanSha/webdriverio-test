5e815b7352d344e4ae12a877a6cebc4a264a8944
Mohan Sha <mohan@webaction.com>
2021-01-09 16:31:21 +0530

    update test runner config

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,14 +50,6 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
-        chromeOptions: {
-            args: [
-                  '--headless',
-                  '--disable-gpu',
-                  '-no-sandbox'
-            ]},
-        privileged: true,
         //
         browserName: 'chrome',
         acceptInsecureCerts: true
@@ -97,9 +89,9 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://localhost',
-    port: 9515,
-    path: '/',
+    baseUrl: ' localhost',
+    port: 4444,
+    path: '/wd/hub',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
